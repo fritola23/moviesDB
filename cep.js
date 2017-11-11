da74e2646fafdff8141020c91972883f9d2eef5c
fritola22 <fritola23@gmail.com>
2017-11-11 19:29:42 -0200

    FilmesDB

---
+++ cep.js
@@ -0,0 +1,33 @@
+var load = $('#loading');
+var tit = $('#film');
+load.hide();
+tit.hide();
+
+$("#pesquisar").click(function(){
+
+	load.show();
+
+	var titulo = $("#titulo").val();
+	console.log(titulo);
+
+	if(titulo != ''){
+
+		$.ajax({
+	url: "http://omdbapi.com/?t="+titulo+"&apikey=PlzBanMe",	
+	method: 'GET'
+	}).done(function(data) {
+	$('#filme').html(data.Title);
+	$('#poster').attr('src', data.Poster);
+	load.hide();
+	tit.show();
+});
+
+	}else{
+		alert('Digite um filme!');
+	}
+});
+
+
+
+
+
